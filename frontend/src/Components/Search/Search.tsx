e760fc6aee7b5b24ac6470478e6f772ace6de92b
Sandica Cristian <cristian.sandica@skywindgroup.com>
2024-11-20 10:05:28 +0200

    search functionality

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent } from 'react'
 
 type Props = {}
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
-    const [search, setSearch] = useState<string>('');
+  const [search, setSearch] = useState<string>('');
 
-    const onClick = (e: any) => {
-        setSearch(e.target.value)
-        console.log(e);
-    }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+    console.log(e);
+  }
 
-  return (
-    <input type="text" value={search} onChange={(e) => onClick(e)} />
-  )
+  const onClick = (e: SyntheticEvent) => {
+
+  }
+
+  return <>
+    <input type="text" value={search} onChange={(e) => handleChange(e)} />
+    <button onClick={(e) => onClick(e)} />
+  </>
+
 }
 
 export default Search
